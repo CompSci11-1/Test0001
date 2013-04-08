c0ea1924f1b8dddaf7b2b49ea06aaf8ac400adc3
Kevin Lawrence <kevin.lawrence@brentwood.bc.ca>
2013-04-08 14:27:25 -0700

    Created Test0001 project body

---
+++ src/test0001/Test0001.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test0001;
+
+/**
+ *
+ * @author kevin.lawrence
+ */
+public class Test0001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test program");
+    }
+}
